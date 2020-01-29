0b3a68dca3b856ffe1776cce13c0e82d4a88273e
Victor Suciu <victor@Victors-MacBook-Pro-4.local>
2020-01-28 18:55:10 -0800

    Fixed bug that couldn't click items when chrome window was too narrow

--- Laptop_Status.py
+++ Laptop_Status.py
@@ -19,11 +19,13 @@ duedate_selector = '#pageBeandueDate'
 num_id = 'pageBeandescription'
 alma_url = 'https://na01.alma.exlibrisgroup.com/mng/login?institute=01ALLIANCE_UW&auth=SAML'
 search_bar_selector = '#ALMA_MENU_TOP_NAV_Search_Text'
+physical_selector = '#record_2_results > div > div.tabsContainer > div.tabsContainerNotExpanded > div:nth-child(1) > div > div > ul > li.tab.hasContentInd.hasContent.jsToolTip.closeOnClick.scrollInit'
+items_in_physical_selector = '#ADD_HIDERADIO_results_1_inventoryLookAheadphysicalUiHoldingResults_csearchbib_resultsnav_pane_physical_upper_actions_items > div > input'
 user_id = 'weblogin_netid'
 user_pass = 'weblogin_password'
 id_subtractor = 45
 
-driver = webdriver.Chrome(executable_path='chromedriver')
+driver = webdriver.Chrome(executable_path='driver/chromedriver')
 driver.get(alma_url)
 actions = ActionChains(driver)
 
@@ -37,8 +39,23 @@ def search_uwb_laptops():
 
 
 def click_items():
-    WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, items_btn_selector)))
-    driver.find_element_by_css_selector(items_btn_selector).click()
+    try:
+        WebDriverWait(driver, 3).until(EC.element_to_be_clickable((By.CSS_SELECTOR, items_btn_selector)))
+        driver.find_element_by_css_selector(items_btn_selector).click()
+    except:
+
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight)")
+        time.sleep(1)
+        WebDriverWait(driver, 4).until(EC.element_to_be_clickable((By.CSS_SELECTOR, physical_selector)))
+        driver.find_element_by_css_selector(physical_selector).click()
+        #driver.execute_script('document.querySelector("' + physical_selector + '").click()')
+        
+        time.sleep(1)
+
+        WebDriverWait(driver, 4).until(EC.presence_of_element_located((By.CSS_SELECTOR, items_in_physical_selector)))
+        #driver.find_element_by_css_selector(items_in_physical_selector).click()
+        driver.execute_script('document.querySelector("' + items_in_physical_selector + '").click()')
+        time.sleep(2)
 
 
 def wait_to_load():
