055470d652ce8e6e098aef5e9b5dfa528205c613
Victor Suciu <victor@Victors-MacBook-Pro-4.local>
2020-01-30 20:55:44 -0800

    Should now handle the number of laptops in the system being changed. Edited date format to make it easier to read.

--- Laptop_Status.py
+++ Laptop_Status.py
@@ -76,6 +76,27 @@ def convert_to_12hr(time_str):
     
     return final_time
 
+def format_date(unformatted):
+    date_list = unformatted.split('/')
+    month = str(int(date_list[0]))
+    day = date_list[1]
+    if len(month) == 1:
+        month = " " + month
+    return month + '-' + day
+
+
+def click_next_page():
+    driver.execute_script("window.scrollTo(0, document.body.scrollHeight)")
+    WebDriverWait(driver, 2).until(EC.presence_of_element_located((By.CSS_SELECTOR, page2_selector)))
+    time.sleep(2)
+    driver.execute_script('document.querySelector("' + page2_selector + '").click()')
+    time.sleep(3)
+
+def print_stack(stack):
+    for i in range(len(stack)):
+        print(stack.pop()),
+
+
 
 search_uwb_laptops()
 click_items()
@@ -88,51 +109,61 @@ try:
     driver.execute_script("window.scrollTo(0, document.body.scrollHeight)")
     time.sleep(2)
     driver.execute_script('document.querySelector("' + page3_selector + '").click()')
-    time.sleep(1)
+    time.sleep(3)
 except:
     is_multi_page = False
 
-print 
 
-for i in range(25):
-    if is_multi_page and i == 6:
-        id_subtractor = 25
-        WebDriverWait(driver, 2).until(EC.presence_of_element_located((By.CSS_SELECTOR, page2_selector)))
-        driver.execute_script("window.scrollTo(0, document.body.scrollHeight)")
-        time.sleep(2)
-        driver.execute_script('document.querySelector("' + page2_selector + '").click()')
-        time.sleep(1)
+output_stack = []
+count = -1
+already_next_page = False
 
-    infolink_id = 'INPUT_SELENIUM_ID_list_ROW_' + str(id_subtractor - i) + '_COL_processTypeForDisplay'
+while(True):
+
+    count += 1
+
+    infolink_id = 'INPUT_SELENIUM_ID_list_ROW_' + str(count) + '_COL_processTypeForDisplay'
+    barcode_id = 'INPUT_SELENIUM_ID_list_ROW_' + str(count) + '_COL_barcode'
+    table_num_id = 'SPAN_SELENIUM_ID_list_ROW_' + str(count) + '_COL_description'
+
     
-    laptop_num = str(i+1) + ": "
-    if i < 9:
-        laptop_num += " "
+    try:
+        WebDriverWait(driver, 2).until(EC.presence_of_element_located((By.ID, table_num_id)))
+        laptop_num = driver.find_element_by_id(table_num_id).get_attribute('innerHTML').split('#')
+        if len(laptop_num) != 2:
+            continue
+        laptop_num = str(int(laptop_num[1]))
+    except:
+        print_stack(output_stack)
+        if is_multi_page and not already_next_page:
+            click_next_page()
+            count = 0
+            already_next_page = True
+            continue
+        else:
+            break
 
     try:
-        WebDriverWait(driver, 2).until(EC.presence_of_element_located((By.ID, infolink_id)))
+        WebDriverWait(driver, 1).until(EC.presence_of_element_located((By.ID, infolink_id)))
         driver.execute_script('document.getElementById("' + infolink_id + '").click()')
+
+        WebDriverWait(driver, 3).until(EC.presence_of_element_located((By.ID, num_id)))
 
         WebDriverWait(driver, 3).until(EC.presence_of_element_located((By.CSS_SELECTOR, duedate_selector)))
         duedate_info = driver.find_element_by_css_selector(duedate_selector).get_attribute('title').split(' ')
 
-        
-        
-        date = duedate_info[0]
+        date = format_date(duedate_info[0])
         time_due = convert_to_12hr(duedate_info[1])
 
-        if(time_due == '11:59:00 PM'):
+        if(time_due == '11:59 PM'):
             time_due += ' <-- Change Due Date'
-            
-        print(laptop_num + date + " | " + time_due)
+           
+        output_stack.append(laptop_num + ': ' + date + ' | ' + time_due + '\n')
 
         WebDriverWait(driver, 3).until(EC.element_to_be_clickable((By.CSS_SELECTOR, backbtn_selector)))
         driver.find_element_by_css_selector(backbtn_selector).click()
 
-    except TimeoutException:
-        print(laptop_num + "IN")
-    
-    print
-
+    except:
+        output_stack.append(laptop_num + ': IN\n')
 
 driver.close()
